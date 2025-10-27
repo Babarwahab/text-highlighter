1023aef81c084aea417e70f8c4c759cd119d4cf4
babar-wahab <48325299+Babarwahab@users.noreply.github.com>
2025-10-27 19:17:58 +0500

    Update TextFileHighlighter.jsx

--- src/TextFileHighlighter.jsx
+++ src/TextFileHighlighter.jsx
@@ -157,14 +157,14 @@ export default function TextFileHighlighter() {
           title="Click to remove highlight"
         >
           {content.slice(start, end)}
-          <svg onClick={() => handleHighlightClick(start, end)} id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 122.88" width={8} height={8} style={{ marginLeft: 2, verticalAlign: "top", cursor: "pointer" }}>
+          {/* <svg onClick={() => handleHighlightClick(start, end)} id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 122.88" width={8} height={8} style={{ marginLeft: 2, verticalAlign: "top", cursor: "pointer" }}>
             <defs></defs>
             <title>cross</title>
             <path
               fill="#ff4141"
               d="M6,6H6a20.53,20.53,0,0,1,29,0l26.5,26.49L87.93,6a20.54,20.54,0,0,1,29,0h0a20.53,20.53,0,0,1,0,29L90.41,61.44,116.9,87.93a20.54,20.54,0,0,1,0,29h0a20.54,20.54,0,0,1-29,0L61.44,90.41,35,116.9a20.54,20.54,0,0,1-29,0H6a20.54,20.54,0,0,1,0-29L32.47,61.44,6,34.94A20.53,20.53,0,0,1,6,6Z"
             />
-          </svg>
+          </svg> */}
         </span>
       );
       last = end;
